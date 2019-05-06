1b171a591e4522340eff3cd24900df86c2aebccb
roy-ermers <dangergamesnl@gmail.com>
2019-05-06 12:25:43 +0200

    added nameform

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,18 @@
     <img alt="Vue logo" src="./assets/Slemp_Logo.png" v-else class="logo">
 
     <start-button v-if="start" v-on:Start="start = false;"/>
+    <name-form v-if="!start"/>
   </div>
 </template>
 
 <script>
 import startButton from "./components/startButton";
+import nameForm from "./components/nameForm";
 export default {
   name: "app",
   components: {
-    startButton
+    startButton,
+    nameForm
   },
   data: () => {
     return { start: true };
---
+++ src/components/nameForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div :key="name" v-for="(name,i) in names" class="name">
+      <input type="text" v-model.lazy="names[i]" :id="name">
+    </div>
+    <button @click="names.push('');">Add</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "nameForm",
+  data: () => {
+    return { names: [""] };
+  }
+};
+</script>
